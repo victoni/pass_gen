cd2365fdd79e1a01baec0519b903ae914d3dc0b7
Victor Markopoulos <vicmarkopoulos@gmail.com>
2018-10-21 16:19:42 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Password Generator
+Small script that generates a password that satisfies the conditions (uppercase letters, lowercase letters, symbols, numbers) based on an easier, universal one.
+(basically a seemingly random ascii string of printable characters)
+
+Examples:
+``` 
+python3 pass_gen.py yahoo password
+P)BIMO:3p8
+
+python3 pass_gen.py freecodecamp password
+=:??AE=0:)GJa12
+
+python3 pass_gen.py www.github.com password
+N?QeE?M3L*e=MCn14
+ 
+```
+
+Passwords were tested [here](http://www.passwordmeter.com)
+
+ Any contribution/criticism is welcome :grimacing: 
+
---
+++ pass_gen.py
@@ -0,0 +1,49 @@
+import sys
+
+#bringing domain and password to equal length, in case domain's length is greater than the password's length --> facebook adminadm
+def dBigger(domain, password):
+	for i in range(0,len(domain)-len(password)):
+		password += password[i]
+	return password
+#bringing domain and password to equal length, in case password's length is greater than the domain's length --> redditre password
+def pBigger(domain, password):
+	for i in range(0,len(password)-len(domain)):
+		domain += domain[i]
+	return domain
+
+#(ascii value domain's char + ascii value of pass's char) mod 33
+def passGen(domain, password):
+	less, most= 33, 126
+	genPass = ''
+	for i in range(0,len(domain)):
+		genPass += chr(((ord(password[i]) + ord(domain[i])) % 93) + 33)
+	for x in genPass:
+		if (ord(x) >= 65) and (ord(x) <= 90):
+			genPass += chr(ord(x)+32) + str(len(genPass))
+			break
+	return genPass
+
+
+def main():
+	if((len(sys.argv) < 3) or(len(sys.argv) > 3)):
+		print('''
+Use: python pass_gen.py [domain] [password]
+Example: 
+python pass_gen.py facebook greatpassword
+4:/-=F7E@?9>-''')
+		sys.exit()
+
+	domain = sys.argv[1]
+	password = sys.argv[2]
+	npass = ''
+	if len(domain) > len(password):
+		password = dBigger(domain, password)
+	elif len(domain) < len(password):
+		domain = pBigger(domain, password)
+	else:
+		pass
+	return print(passGen(domain, password))
+
+
+if __name__ == '__main__':
+	main()
