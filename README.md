98568b66330fc0f80721abc77742fcfa7e965df9
Viktor Markopoulos <vicmarkopoulos@gmail.com>
2020-05-20 11:00:04 +0200

    added argparse and hidden mode

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # Password Generator
 Small script that generates a password that satisfies the conditions (uppercase letters, lowercase letters, symbols, numbers) based on an easier, universal one.
-(basically a seemingly random ascii string of printable characters)
+
+```
+usage: pass_gen.py [-h] [-d D] [-c]
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -d D        the domain for the password
+  -c          copies the generated password into your clipboard without printing it
+```
 
 Examples:
 ``` 
-python3 pass_gen.py yahoo
+$ python3 pass_gen.py -d yahoo
 Your bad password: password
 Your password:
 
 P)BIMO:3p8
 
+```
+```
+$ python3 pass_gen.py -d yahoo -c
+Your bad password: [typing]
+$ #copied into your clipboard
 ```
 
 `password` can be cracked instantly using wordlists.
--- pass_gen.py
+++ pass_gen.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
-import sys
+import sys, pyperclip, argparse
+from getpass import getpass
 
 #bringing domain and password to equal length
 def func(domain, password):
@@ -22,26 +22,27 @@ def passGen(domain, password):
 
 
 def main():
-	if((len(sys.argv) < 2) or(len(sys.argv) > 2)):
-		print('''
-Use: python {0} [domain]\n
-Example: 
-python3 {0} facebook
-Enter your bad password: password
-Your password:
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-d", help="the domain for the password")
+	parser.add_argument("-c", help="copies the generated password into your clipboard without printing it", action="store_true")
+	args = parser.parse_args()
 
-=)=?@EH6e8
-'''.format(sys.argv[0]))
-		sys.exit()
+	if args.d is None:
+		parser.parse_args(['-h'])
+	domain = args.d
+	password = getpass("Enter your bad password: ")
+	
 
-	domain = sys.argv[1]
-	password = input("Enter your bad password: ")
 	if len(domain) > len(password):
 		password = func(domain, password)
 	elif len(domain) < len(password):
 		domain = func(password, domain)
 	else:
 		pass
+	
+
+	if args.c:
+		return pyperclip.copy(passGen(domain, password))
 	return print("Your password:\n\n" + passGen(domain, password) + "\n")
 
 
